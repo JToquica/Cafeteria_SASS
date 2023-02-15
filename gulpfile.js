cd3cf28063d7bf5b49667ab90387f652bade9cb6
JToquica <jose.herley.2010@gmail.com>
2023-02-15 16:47:52 -0500

    Proyecto Terminado

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 // IMAGENES
 const imagemin = require('gulp-imagemin');
@@ -15,8 +17,10 @@ function css(done) {
     // Compilar SASS
     //1. Identificar archivo (css), 2 Compilar, 3. Guardar .CSS
     src('src/scss/app.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'))
 
     done();
